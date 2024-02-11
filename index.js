f8599d40804a633b901a3723b30ff4e606a99303
Ray <buttrafay980@gmail.com>
2024-02-11 21:14:53 +0500

    updating header

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors = require( "cors" );
 const bodyParser = require( "body-parser" );
 const { ANIME } = require( "@consumet/extensions" );
 const zoro = new ANIME.Zoro();
-
+var https = require( "http" );
+var server = https.createServer( app );
 const port = 5260;
 
 
@@ -21,6 +22,7 @@ const searchAnime = async ( query ) => {
 
 
 app.use( bodyParser.json() );
+app.use( express.json() );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( cors() );
 
@@ -30,10 +32,13 @@ app.get( "/", ( req, res ) => {
 
 app.post( "/search", async ( req, res ) => {
   const query = req.body.query; // assuming the client sends a 'query' parameter in the request body
+  console.log( query );
+  console.log( req.body );
   try {
     const searchResults = await searchAnime( query );
-
+    console.log( query );
     if ( searchResults ) {
+      console.log( searchResults );
       res.json( searchResults ); // sending the search results back to the client
     } else {
       res.status( 404 ).json( { error: "No results found" } );
@@ -48,4 +53,4 @@ app.post( "/search", async ( req, res ) => {
 
 
 
-app.listen( port, () => console.log( `server running on ${ port }` ) );
+server.listen( port, () => console.log( `server running on ${ port }` ) );
