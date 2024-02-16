e35d568707007647f66e6ba887082cc0c5b823f0
Ray <buttrafay980@gmail.com>
2024-02-16 16:29:41 +0500

    updating view

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require( "dotenv" ).config();
+
 const express = require( "express" );
+const { createClient } = require( '@supabase/supabase-js' );
 const fs = require( "fs/promises" );
 const app = express();
 const cors = require( "cors" );
@@ -7,12 +10,42 @@ const { ANIME } = require( "@consumet/extensions" );
 var https = require( "http" );
 var server = https.createServer( app );
 const gogo = new ANIME.Gogoanime();
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const port = 5260;
-
-
+let cachedValue = 10;
+const supabase = createClient( supabaseUrl, supabaseKey );
 
 
 gogo.baseUrl = "https://anitaku.to";
+
+
+async function updateCachedValue () {
+  try {
+    const { data, error } = await supabase
+      .from( 'sp' )
+      .update( { viewers: cachedValue } )
+      .eq( 'id', 1 )
+      .select();
+
+    if ( error ) {
+      throw error;
+    }
+
+    if ( data.length === 0 ) {
+      throw new Error( 'No rows found' );
+    }
+
+    cachedValue = parseInt( data[ 0 ].viewers );
+  } catch ( error ) {
+    console.error( 'Error fetching value from Supabase:', error );
+  }
+}
+
+updateCachedValue();
+
+setInterval( updateCachedValue, 60000 );
+
 
 const searchAnime = async ( query, page = 1 ) => {
   let results = await gogo.search( query, page );
@@ -136,9 +169,7 @@ app.post( "/search", async ( req, res ) => {
 app.get( "/num-of-viewers", async ( _, res ) => {
   try {
 
-    const siteData = await fs.readFile( "./siteData.txt" ).then( num => num.toString() );
-    if ( siteData ) res.json( { siteData } );
-    else res.status( 404 ).send( "error" );
+    res.send( cachedValue );
 
   } catch ( e ) {
     console.log( e );
