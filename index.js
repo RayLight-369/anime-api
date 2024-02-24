88aa79430fb92410ceac3dcdb7e66f87f16ee16c
Ray <buttrafay980@gmail.com>
2024-02-24 18:14:31 +0500

    updating providers

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require( "express" );
 const { createClient } = require( '@supabase/supabase-js' );
 const fs = require( "fs/promises" );
 const TorrentSearchApi = require( "torrent-search-api" );
+const cheerio_1 = require( "cheerio" );
 const app = express();
 TorrentSearchApi.enableProvider( "Yts" );
 const cors = require( "cors" );
@@ -11,7 +12,13 @@ const bodyParser = require( "body-parser" );
 const { ANIME } = require( "@consumet/extensions" );
 var https = require( "http" );
 var server = https.createServer( app );
+
+// const ajaxUrl = "https://ajax.gogo-load.com/ajax";
+// const baseUrl = "https://anitaku.to";
+
 const gogo = new ANIME.Gogoanime();
+gogo.fetchAnimeInfo = fetchAnimeInfo.bind( gogo );
+
 const torrentGalaxy = require( './torrent/torrentGalaxy' );
 const limeTorrent = require( './torrent/limeTorrent' );
 
@@ -23,6 +30,92 @@ const supabase = createClient( supabaseUrl, supabaseKey );
 
 
 gogo.baseUrl = "https://anitaku.to";
+
+
+
+async function fetchAnimeInfo ( id ) {
+  if ( !id.includes( 'gogoanime' ) )
+    id = `${ this.baseUrl }/category/${ id }`;
+  const animeInfo = {
+    id: '',
+    title: '',
+    url: '',
+    genres: [],
+    totalEpisodes: 0,
+  };
+
+  // try {
+  const res = await this.client.get( id );
+  const $ = ( 0, cheerio_1.load )( res.data );
+  animeInfo.id = new URL( id ).pathname.split( '/' )[ 2 ];
+  animeInfo.title = $( 'section.content_left > div.main_body > div:nth-child(2) > div.anime_info_body_bg > h1' )
+    .text()
+    .trim();
+  animeInfo.url = id;
+  animeInfo.image = $( 'div.anime_info_body_bg > img' ).attr( 'src' );
+  animeInfo.releaseDate = $( 'div.anime_info_body_bg > p:nth-child(8)' )
+    .text()
+    .trim()
+    .split( 'Released: ' )[ 1 ];
+  animeInfo.description = $( 'div.anime_info_body_bg > div.description' )
+    .text()
+    .trim()
+    .replace( 'Plot Summary: ', '' );
+  animeInfo.subOrDub = animeInfo.title.toLowerCase().includes( 'dub' ) ? "dub" : "sub";
+  animeInfo.type = $( 'div.anime_info_body_bg > p:nth-child(4) > a' )
+    .text()
+    .trim()
+    .toUpperCase();
+  animeInfo.status = "Unknown";
+  switch ( $( 'div.anime_info_body_bg > p:nth-child(9) > a' ).text().trim() ) {
+    case 'Ongoing':
+      animeInfo.status = "Ongoing";
+      break;
+    case 'Completed':
+      animeInfo.status = "Completed";
+      break;
+    case 'Upcoming':
+      animeInfo.status = "Not yet aired";
+      break;
+    default:
+      animeInfo.status = "Unknown";
+      break;
+  }
+  animeInfo.otherName = $( 'div.anime_info_body_bg > p.other-name' )
+    .text()
+    .replace( 'Other name: ', '' )
+    .replace( /;/g, ',' );
+  $( 'div.anime_info_body_bg > p:nth-child(7) > a' ).each( ( i, el ) => {
+    var _a;
+    ( _a = animeInfo.genres ) === null || _a === void 0 ? void 0 : _a.push( $( el ).attr( 'title' ).toString() );
+  } );
+
+  const ep_start = $( '#episode_page > li' ).first().find( 'a' ).attr( 'ep_start' );
+  const ep_end = $( '#episode_page > li' ).last().find( 'a' ).attr( 'ep_end' );
+  const movie_id = $( '#movie_id' ).attr( 'value' );
+  const alias = $( '#alias_anime' ).attr( 'value' );
+  const html = await this.client.get( `${ this.ajaxUrl }/load-list-episode?ep_start=${ ep_start }&ep_end=${ ep_end }&id=${ movie_id }&default_ep=${ 0 }&alias=${ alias }` );
+  const $$ = ( 0, cheerio_1.load )( html.data );
+  animeInfo.episodes = [];
+
+  $$( '#episode_related > li' ).each( ( i, el ) => {
+    var _a, _b, _c;
+    ( _a = animeInfo.episodes ) === null || _a === void 0 ? void 0 : _a.push( {
+      id: ( _b = $( el ).find( 'a' ).attr( 'href' ) ) === null || _b === void 0 ? void 0 : _b.split( '/' )[ 1 ],
+      number: parseFloat( $( el ).find( `div.name` ).text().replace( 'EP ', '' ) ),
+      url: `${ this.baseUrl }/${ ( _c = $( el ).find( `a` ).attr( 'href' ) ) === null || _c === void 0 ? void 0 : _c.trim() }`,
+    } );
+  } );
+  animeInfo.episodes = animeInfo.episodes.reverse();
+  animeInfo.totalEpisodes = parseInt( ep_end !== null && ep_end !== void 0 ? ep_end : '0' );
+  return animeInfo;
+  // }
+  // catch ( err ) {
+  //   throw new Error( `failed to fetch anime info: ${ err }` );
+  // }
+};
+
+
 
 async function updateCachedValue ( update ) {
   try {
@@ -129,7 +222,7 @@ app.post( "/anime-info", async ( req, res ) => {
     else res.status( 404 ).json( { error: "Invalid ID" } );
 
   } catch ( e ) {
-    ////console.log( e );
+    // console.log( e );
     res.status( 500 ).json( { error: "Internal server error" } );
   }
 } );
